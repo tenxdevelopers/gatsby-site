d7a2230cb6d127406e75b436176146d078bdc912
TenxDevelopers <support@tenxdevelopers.com>
2021-09-02 12:20:36 -0700

    render dynamic pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+
+import { graphql } from 'gatsby'
+
+import Layout from '../../components/layout'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+const BlogPost = ({data}) => {    
+    return (
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <p>{data.mdx.frontmatter.date}</p>
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
+        </Layout>
+    )
+}
+
+
+
+export const query = graphql `
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                date(formatString: "MMM D, YYYY")
+                title
+            }        
+            body
+        }
+    }
+`
+
+export default BlogPost
