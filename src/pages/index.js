8d6562f61a82b382d6c29cd96c4a2822d70f79f3
TenxDevelopers <support@tenxdevelopers.com>
2021-09-02 10:16:51 -0700

    Grogu pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from 'react'
 
+import { StaticImage } from 'gatsby-plugin-image'
+
 import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making the following using the Gatsby Tutorial</p>
+      <StaticImage src="../images/Grogu.webp" alt="Grogu Image"></StaticImage>
     </Layout>
   )
 }
